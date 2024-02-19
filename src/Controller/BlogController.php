f390d64208ac02a10c3bf2f54cdaa69436cee277
Hana Ben Amor <benamor.hana@issatso.u-sousse.tn>
2024-02-19 14:30:40 +0100

    first commit

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+
+class BlogController extends AbstractController
+{
+    #[Route('/blog', name: 'app_blog')]
+    public function index(ArticleRepository $repo): Response
+    {
+        $articles=$repo->findAll('Titre de l\'article');
+        Return $this->render('blog/index.html.twig', ['controller_name' =>
+        'BlogController','articles'=>$articles,]);
+        
+    }
+
+    #[Route('/blog/{id}', name: 'blog_show')]
+public function show($id, ArticleRepository $repo)
+{
+$article = $repo->find($id);
+return $this->render('blog/show.html.twig',['article'=>$article]);
+}
+}
